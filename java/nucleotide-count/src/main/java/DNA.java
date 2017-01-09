7c38556d9349fd2a96204c6cf6194cc563ab9edd
Karina Gonzalez <karinamzalez@gmail.com>
2017-01-09 16:59:18 -0700

    first test in bob.spec.js is pasing

--- java/nucleotide-count/src/main/java/DNA.java
+++ java/nucleotide-count/src/main/java/DNA.java
@@ -2,7 +2,7 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class DNA {
- // String reqDNA = "ACGT";
+ String reqDNA = "ACGT";
 
   public DNA(String strand){
     String dnaStrand = strand;
@@ -11,26 +11,26 @@ public class DNA {
   public int count(char nucleobase){
     int count = 0;
     return count;
-  // if (reqDNA.indexOf(nucleobase) < 0) {
-  //   throw new IllegalArgumentException();
+  if (reqDNA.indexOf(nucleobase) < 0) {
+    throw new IllegalArgumentException();
   }
 
- //    for (char c : dnaString.toCharArray()) {
- //   if (c == needle) {
- //    count++;
- //   }
- //  }
- //    return count;
- // }
- //
- // public Map<Character, Integer> nucleotideCounts(){
- // Map<Character, Integer> dnaMap = new HashMap<Character, Integer>();
- //
- //   dnaMap.put('A', count('A'));
- //   dnaMap.put('C', count('C'));
- //   dnaMap.put('G', count('G'));
- //   dnaMap.put('T', count('T'));
- //
- //  return dnaMap;
- // }
+    for (char c : dnaString.toCharArray()) {
+   if (c == needle) {
+    count++;
+   }
+  }
+    return count;
+ }
+
+ public Map<Character, Integer> nucleotideCounts(){
+ Map<Character, Integer> dnaMap = new HashMap<Character, Integer>();
+
+   dnaMap.put('A', count('A'));
+   dnaMap.put('C', count('C'));
+   dnaMap.put('G', count('G'));
+   dnaMap.put('T', count('T'));
+
+  return dnaMap;
+ }
 }
---
+++ javascript/bob/bob.js
@@ -0,0 +1,16 @@
+//
+// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+var Bob = function() {};
+
+Bob.prototype.hey = function(input) {
+  if (input.includes('!')) {
+    return 'Woah, chill out!';
+  } else {
+    return 'Whatever.';
+  }
+};
+
+module.exports = Bob;
