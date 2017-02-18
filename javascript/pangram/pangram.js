664db4a42acdd68ba2fc4e11c084c9728346d512
Karina Gonzalez <karinamzalez@gmail.com>
2017-02-17 18:12:42 -0600

    pangram -- ALL TESTS PASSING

---
+++ javascript/pangram/pangram.js
@@ -0,0 +1,44 @@
+var Pangram = function(string) {
+  this.sentence = string;
+  this.alphabet = {a:false, b:false, c:false,
+                   d:false, e:false,
+                   f:false, g:false, h:false,
+                   i:false, j:false, k:false,
+                   l:false, m:false, n:false,
+                   o:false, p:false, q:false,
+                   r:false, s:false, t:false,
+                   u:false, v:false, w:false,
+                   x:false, y:false, z:false};
+};
+
+Pangram.prototype.isPangram = function() {
+  var pangram = true;
+  var sentence = this.sentence;
+  var alphabet = this.alphabet;
+  parseSentence(sentence, alphabet);
+  for (var key in alphabet) {
+    if (alphabet[key] === false) {
+      pangram = false;
+      break;
+    }
+  }
+  return pangram;
+};
+
+function parseSentence(sentence, alphabet) {
+  for (var i = 0; i < sentence.length; i++) {
+    var letter = sentence[i];
+    var lowercaseLetter = letter.toLowerCase();
+    markTrueInAlphabetArray(lowercaseLetter, alphabet);
+  }
+}
+
+function markTrueInAlphabetArray(letter, alphabet) {
+  for (var key in alphabet) {
+    if (key === letter){
+      alphabet[key] = true;
+    }
+  }
+}
+
+module.exports = Pangram;
