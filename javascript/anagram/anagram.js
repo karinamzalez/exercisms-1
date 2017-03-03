53e630e22e17b82aae3bb44dcb2bddc7412782ce
Karina Gonzalez <karinamzalez@gmail.com>
2017-03-03 14:48:43 -0700

    anagram-- all tests passing

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,22 +1,44 @@
 var Anagram = function(word) {
-  this.word = word;
+  this.word = word.toLowerCase();
 };
 
-Anagram.prototype.matches = function(potentialMatches) {
-  var lettersHash =  wordToHash(this.word);
+Anagram.prototype.matches = function() {
+  var potentialMatches = [];
+  if (!(arguments[0] instanceof Array)) {
+    potentialMatches = [];
+    for (var i = 0; i < arguments.length; ++i) {
+      var word = arguments[i];
+      potentialMatches.push(word.toLowerCase());
+    }
+  } else {
+    potentialMatches = arguments[0];
+  }
+
+  return buildMatchesArray(potentialMatches, this.word);
+};
+
+function buildMatchesArray(potentialMatches, word) {
+  var lettersHash =  wordToHash(word);
   var matches = [];
   for (var i = 0; i < potentialMatches.length; i++) {
-    if (match(lettersHash, potentialMatches[i])) {
-      matches.push(potentialMatches[i]);
+    var potentialMatch = potentialMatches[i];
+    var lowerCasePotentialMatch = potentialMatch.toLowerCase();
+    if (match(lettersHash, lowerCasePotentialMatch) &&
+        lowerCasePotentialMatch !== word) {
+      matches.push(potentialMatch);
     }
-    lettersHash = wordToHash(this.word);
+    lettersHash = wordToHash(word);
   }
   return matches;
-};
+}
 
 function wordToHash(word) {
   var lettersHash = {};
   for (var i = 0; i < word.length; i++) {
+    if (word[i] in lettersHash) {
+      var key = word[i] + "1";
+      lettersHash[key] = false;
+    }
     lettersHash[word[i]] = false;
   }
   return lettersHash;
@@ -24,10 +46,20 @@ function wordToHash(word) {
 
 function match(lettersHash, potentialMatch) {
   var match = true;
+  var letters = [];
   for (var i = 0; i < potentialMatch.length; i++) {
     var letter = potentialMatch[i];
-    if (letter in lettersHash) {
+    letters.push(letter);
+    if (lettersHash[letter] === true){
+      if ((letter + "1") in lettersHash) {
+        lettersHash[letter + "1"] = true;
+      } else {
+        match = false;
+      }
+    } else if (letter in lettersHash) {
       lettersHash[letter] = true;
+    } else if ((letter + "1") in lettersHash) {
+      lettersHash[letter + "1"] = true;
     } else {
       match = false;
     }
