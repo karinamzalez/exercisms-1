fcbb1208477db7f7b7d4f0aa9731be66ac36e6b1
Karina Muñoz Gonzalez <karinamzalez@gmail.com>
2017-04-03 18:19:24 -0600

    all tests passing

--- java/nucleotide-count/src/main/java/DNA.java
+++ java/nucleotide-count/src/main/java/DNA.java
@@ -1,36 +1,38 @@
 import java.util.Map;
 import java.util.HashMap;
 
-public class DNA {
- // String reqDNA = "ACGT";
+public final class DNA {
+  private final String strand;
+  private static final String validNucleotides = "AGCT";
 
   public DNA(String strand){
-    String dnaStrand = strand;
+    this.strand = strand;
   }
 
-  public int count(char nucleobase){
-    int count = 0;
+  public Integer count(char nucleobase){
+    Integer count = 0;
+
+    if (validNucleotides.indexOf(nucleobase) == -1) {
+      throw new IllegalArgumentException();
+    }
+
+    for (char c : strand.toCharArray()) {
+      if (c == nucleobase) {
+        count++;
+      }
+    }
+
     return count;
-  // if (reqDNA.indexOf(nucleobase) < 0) {
-  //   throw new IllegalArgumentException();
-  }
+ }
 
- //    for (char c : dnaString.toCharArray()) {
- //   if (c == needle) {
- //    count++;
- //   }
- //  }
- //    return count;
- // }
- //
- // public Map<Character, Integer> nucleotideCounts(){
- // Map<Character, Integer> dnaMap = new HashMap<Character, Integer>();
- //
- //   dnaMap.put('A', count('A'));
- //   dnaMap.put('C', count('C'));
- //   dnaMap.put('G', count('G'));
- //   dnaMap.put('T', count('T'));
- //
- //  return dnaMap;
- // }
+  public Map<Character, Integer> nucleotideCounts(){
+    Map<Character, Integer> dnaMap = new HashMap<Character, Integer>();
+
+    dnaMap.put('A', count('A'));
+    dnaMap.put('C', count('C'));
+    dnaMap.put('G', count('G'));
+    dnaMap.put('T', count('T'));
+
+    return dnaMap;
+  }
 }
--- java/nucleotide-count/src/test/java/NucleotideTest.java
+++ java/nucleotide-count/src/test/java/NucleotideTest.java
@@ -14,7 +14,7 @@ public class NucleotideTest {
         assertThat(dna.count('A'), is(0));
     }
 
-    @Ignore
+    @Test
     public void testEmptyDnaStringHasNoNucleotides() {
         DNA dna = new DNA("");
         Map<Character, Integer> counts = dna.nucleotideCounts();
@@ -26,77 +26,70 @@ public class NucleotideTest {
                 hasEntry('T', 0)
         ));
     }
-    //
-    // @Ignore
-    // @Test
-    // public void testRepetitiveCytidineGetsCounted() {
-    //     DNA dna = new DNA("CCCCC");
-    //     assertThat(dna.count('C'), is(5));
-    // }
-    //
-    // @Ignore
-    // @Test
-    // public void testRepetitiveSequenceWithOnlyGuanosine() {
-    //     DNA dna = new DNA("GGGGGGGG");
-    //     Map<Character, Integer> counts = dna.nucleotideCounts();
-    //     assertThat(counts.size(), is(4));
-    //     assertThat(counts, allOf(
-    //             hasEntry('A', 0),
-    //             hasEntry('C', 0),
-    //             hasEntry('G', 8),
-    //             hasEntry('T', 0)
-    //     ));
-    // }
-    //
-    // @Ignore
-    // @Test
-    // public void testCountsOnlyThymidine() {
-    //     DNA dna = new DNA("GGGGGTAACCCGG");
-    //     assertThat(dna.count('T'), is(1));
-    // }
-    //
-    // @Ignore
-    // @Test
-    // public void testCountsANucleotideOnlyOnce() {
-    //     DNA dna = new DNA("CGATTGGG");
-    //     dna.count('T');
-    //     assertThat(dna.count('T'), is(2));
-    // }
-    //
-    // @Ignore
-    // @Test
-    // public void testDnaCountsDoNotChangeAfterCountingAdenosine() {
-    //     DNA dna = new DNA("GATTACA");
-    //     dna.count('A');
-    //     Map<Character, Integer> counts = dna.nucleotideCounts();
-    //     assertThat(counts.size(), is(4));
-    //     assertThat(counts, allOf(
-    //             hasEntry('A', 3),
-    //             hasEntry('C', 1),
-    //             hasEntry('G', 1),
-    //             hasEntry('T', 2)
-    //     ));
-    // }
-    //
-    // @Ignore
-    // @Test(expected = IllegalArgumentException.class)
-    // public void testValidatesNucleotides() {
-    //     DNA dna = new DNA("GACT");
-    //     dna.count('X');
-    // }
-    //
-    // @Ignore
-    // @Test
-    // public void testCountsAllNucleotides() {
-    //     String s = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC";
-    //     DNA dna = new DNA(s);
-    //     Map<Character, Integer> counts = dna.nucleotideCounts();
-    //     assertThat(counts.size(), is(4));
-    //     assertThat(counts, allOf(
-    //             hasEntry('A', 20),
-    //             hasEntry('C', 12),
-    //             hasEntry('G', 17),
-    //             hasEntry('T', 21)
-    //     ));
-    // }
+
+    @Test
+    public void testRepetitiveCytidineGetsCounted() {
+        DNA dna = new DNA("CCCCC");
+        assertThat(dna.count('C'), is(5));
+    }
+
+    @Test
+    public void testRepetitiveSequenceWithOnlyGuanosine() {
+        DNA dna = new DNA("GGGGGGGG");
+        Map<Character, Integer> counts = dna.nucleotideCounts();
+        assertThat(counts.size(), is(4));
+        assertThat(counts, allOf(
+                hasEntry('A', 0),
+                hasEntry('C', 0),
+                hasEntry('G', 8),
+                hasEntry('T', 0)
+        ));
+    }
+
+    @Test
+    public void testCountsOnlyThymidine() {
+        DNA dna = new DNA("GGGGGTAACCCGG");
+        assertThat(dna.count('T'), is(1));
+    }
+
+    @Test
+    public void testCountsANucleotideOnlyOnce() {
+        DNA dna = new DNA("CGATTGGG");
+        dna.count('T');
+        assertThat(dna.count('T'), is(2));
+    }
+
+    @Test
+    public void testDnaCountsDoNotChangeAfterCountingAdenosine() {
+        DNA dna = new DNA("GATTACA");
+        dna.count('A');
+        Map<Character, Integer> counts = dna.nucleotideCounts();
+        assertThat(counts.size(), is(4));
+        assertThat(counts, allOf(
+                hasEntry('A', 3),
+                hasEntry('C', 1),
+                hasEntry('G', 1),
+                hasEntry('T', 2)
+        ));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testValidatesNucleotides() {
+        DNA dna = new DNA("GACT");
+        dna.count('X');
+    }
+
+    @Test
+    public void testCountsAllNucleotides() {
+        String s = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC";
+        DNA dna = new DNA(s);
+        Map<Character, Integer> counts = dna.nucleotideCounts();
+        assertThat(counts.size(), is(4));
+        assertThat(counts, allOf(
+                hasEntry('A', 20),
+                hasEntry('C', 12),
+                hasEntry('G', 17),
+                hasEntry('T', 21)
+        ));
+    }
 }
