52f88d4fdfeb5d8b6574fe093036b6a4a3164467
Karina Muñoz Gonzalez <karinamzalez@gmail.com>
2017-04-06 16:18:14 -0600

    add translation hashMap

--- java/rna-transcription/src/main/java/RnaTranscription.java
+++ java/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,4 +1,17 @@
+import java.util.Map;
+import java.util.HashMap;
+
 public class RnaTranscription {
+    private static final Map<String, String> translation = new HashMap<Character, Integer>(){
+      {
+        put("C", "G");
+        put("G", "C");
+        put("T", "A");
+        put("A", "U");
+      }
+    };
+
+
     public String ofDna(String dnaString) {
         if (dnaString == "C") {
           return "G";
