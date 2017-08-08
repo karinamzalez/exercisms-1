d40c390eae79cce906f72b4ba13e14518278eff5
Karina Muñoz Gonzalez <karinamzalez@gmail.com>
2017-08-08 17:12:36 -0600

    wip

--- javascript/simple-cipher/simple-cipher.js
+++ javascript/simple-cipher/simple-cipher.js
@@ -1,9 +1,14 @@
 function Cipher() {
-  this.key = this.generateKey;
+  this.key = this.generateKey();
 }
 
 Cipher.prototype.generateKey = () => {
-
+  var emptyString = "";
+  var alphabet = "abcdefghijklmnopqrstuvwxyz";
+  while (emptyString.length < 6) {
+    emptyString += alphabet[Math.floor(Math.random() * alphabet.length)];
+  }
+  return emptyString;
 };
 
 Cipher.prototype.encode = () => {
