7877507213dbb3fb517e1fead9ecb43565115049
Karina Muñoz Gonzalez <karinamzalez@gmail.com>
2017-08-15 13:01:23 -0600

    add simple cipher soluntion

--- javascript/simple-cipher/simple-cipher.js
+++ javascript/simple-cipher/simple-cipher.js
@@ -1,18 +1,45 @@
-function Cipher() {
-  this.key = this.generateKey();
+function checkKey(key) {
+  if (key !== undefined)  {
+    for (var i = 0; i < key.length; i++) {
+      if (key[i] == key[i].toUpperCase()) {
+        throw "Bad key";
+      }
+    }
+    if (key === "") {
+      throw "Bad key";
+    }
+  }
+  return key;
 }
 
-Cipher.prototype.generateKey = () => {
-  var emptyString = "";
-  var alphabet = "abcdefghijklmnopqrstuvwxyz";
-  while (emptyString.length < 6) {
-    emptyString += alphabet[Math.floor(Math.random() * alphabet.length)];
+class Cipher{
+  constructor(key){
+    this.key = checkKey(key) || "aaaaaaaaaa";
   }
-  return emptyString;
-};
 
-Cipher.prototype.encode = () => {
+  encode(string){
+    var encodedString = "";
+    for (var i = 0; i < string.length; i++) {
+      var encodedCharCode = (string[i].charCodeAt(0) - 97) + (this.key[i].charCodeAt(0) - 97);
+      if (encodedCharCode > 25) {
+        encodedCharCode = encodedCharCode - 26;
+      }
+      var encodedLetter = String.fromCharCode(97 + encodedCharCode);
+      encodedString = encodedString + encodedLetter;
+    }
+    return encodedString;
+  }
 
-};
+  decode(string){
+    var decodedString = "";
+    for (var i = 0; i < string.length; i++) {
+      var decodedCharCode = (string[i].charCodeAt(0) - 97) - (this.key[i].charCodeAt(0) - 97);
+      var decodedLetter = String.fromCharCode(97 + decodedCharCode);
+      decodedString = decodedString + decodedLetter;
+    }
+    return decodedString;
+  }
+}
+
 
 module.exports = Cipher;
